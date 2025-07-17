fb1e589de52f63eb3c0f196e44fcbe4198cd5d63
Mr-Yozz <yogeshwaran03042002@gmail.com>
2025-07-17 06:48:43 +0530

    setting

--- project/settings.py
+++ project/settings.py
@@ -91,9 +91,9 @@ DATABASES = {
     }
 }
 
-
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = '/media/'
 
 # Default primary key field type
